4d4a9531c5bf7f696e265d27be3d43886db54129
geekNero <sg550js@gmail.ciom>
2024-12-01 13:06:21 +0530

    Reverting new testcases for now

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,2 +1,2 @@
 export DEBUG=True && faketime '2024-08-31 21:00:00' python3 -m unittest test_application_layer.py
-export DEBUG=True && chmod +x verify_debug.sh&& cd fresh_install && faketime '2024-08-31 21:00:00' python3 -m unittest test.py
+#export DEBUG=True && chmod +x verify_debug.sh&& cd fresh_install && faketime '2024-08-31 21:00:00' python3 -m unittest test.py
